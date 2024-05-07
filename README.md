741541a082840e7739215e6c988a3ccf2634f287
Bruno Albuquerque <bruno.felipe.albuquerque@gmail.com>
2024-05-07 14:29:21 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# GitHub Portfolio
+
+---
+
+## View site [here](https://brunoalbuquerque.click/)
+
+---
+
+# Packages Used :package:
+
+|   Used Package List   |
+| :-------------------: |
+|         next          |
+|  @next/third-parties  |
+|         axios         |
+|      react-icons      |
+| react-github-calendar |
+|         sass          |
+|      tailwindcss      |
+
+---
+
+## Disclaimer
+
+Heavily inspired by [Abu Said](https://github.com/said7388) [github-portfolio](https://github.com/said7388/github-portfolio).
---
+++ data/user-data.js
@@ -0,0 +1,18 @@
+export const userData = {
+  githubUser: 'BrunoFelipeAlbuquerque',
+  devUsername: "BrunoFelipeAlbuquerque",
+  github: 'https://github.com/BrunoFelipeAlbuquerque',
+  facebook: 'https://www.facebook.com/brunorodriguesalbuquerque/',
+  linkedIn: 'https://www.linkedin.com/in/brunofelipealbuquerque/',
+  twitter: 'https://twitter.com/BrunoFelip52739',
+  stackOverflow: 'https://stackoverflow.com/users/3323543/bruno-albuquerque',
+  leetcode: "https://leetcode.com/u/BrunoFelipeAlbuquerque/",
+  resume: "https://drive.google.com/file/d/13lrH1Iuxijna21edodZWUmfPb51sa5T6/view?usp=sharing",
+  languages: ['asp','golang','javascript','php','typescript'],
+  frameworks: ['cakephp','express','laravel','nestjs','nuxt','symfony','vue'],
+  databases: ['mariadb','mongodb','mysql','oracle','postgresql','redis','sql server'],
+  containerization: ['docker','docker-compose','kubernetes','podman'],
+  clouds: ['aws', 'gcloud'],
+  others: ['amazon sqs','kafka','mosquittomqtt','rabbitmq'],
+  timezone: '-3'
+};
